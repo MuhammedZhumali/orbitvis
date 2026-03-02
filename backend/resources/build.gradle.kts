ca7e7551792c7ca3fcd60e9436264163aa2addcc
Mukhammed <zhumalimuhammed@gmail.com>
2026-03-02 10:01:02 +0500

    draft setup

---
+++ backend/resources/build.gradle.kts
@@ -0,0 +1,47 @@
+plugins {
+    id("org.springframework.boot") version "3.2.5"
+    id("io.spring.dependency-management") version "1.1.4"
+    java
+}
+
+group = "com.example"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_21
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.projectlombok.lombok")
+
+    implementation("org.orekit:orekit:12.0")
+
+    implementation("org.hibernate.orm:hibernate-spatial:6.4.4.Final")
+
+    runtimeOnly("org.postgresql:postgresql")
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+    testCompileOnly("org.projectlombok:lombok")
+    testAnnotationProcessor("org.projectlombok:lombok")
+}
+
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+springBoot {
+    mainClass.set("com.orbitvis.backend.BackendApplication") 
+}
---
+++ settings.grade.kts
@@ -0,0 +1,4 @@
+rootProject.name = "orbitvis"
+
+
+include("backend")
