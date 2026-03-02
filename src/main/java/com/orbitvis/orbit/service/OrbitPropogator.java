051528d5e2ae27268e5f3db32d7ed66372a3cb77
Mukhammed <zhumalimuhammed@gmail.com>
2026-03-02 15:20:09 +0500

    orbit propogation draft

--- src/main/java/com/orbitvis/orbit/service/OrbitPropogator.java
+++ src/main/java/com/orbitvis/orbit/service/OrbitPropogator.java
@@ -14,6 +14,12 @@ import org.orekit.utils.IERSConventions;
 import org.orekit.propagation.analytical.tle.TLE;
 import org.orekit.propagation.analytical.tle.TLEPropagator;
 import org.orekit.propagation.SpacecraftState;
+
+import java.io.File;
+import java.net.URL;
+
+import javax.management.RuntimeErrorException;
+
 import org.hipparchus.geometry.euclidean.threed.Vector3D;
 import org.orekit.time.AbsoluteDate;
 import org.orekit.utils.Constants;
@@ -36,6 +42,7 @@ public class OrbitPropogator {
  
     
     public OrbitPropogator(){
+        initializeOrekit();
         itrf = FramesFactory.getITRF(IERSConventions.IERS_2010, true);
         earth = new OneAxisEllipsoid(
              Constants.WGS84_EARTH_EQUATORIAL_RADIUS,
@@ -43,4 +50,53 @@ public class OrbitPropogator {
             itrf
         );
     }
+
+    private void initializeOrekit(){
+        try{
+            File orekitData = findOrekitDataDirectory();
+            if(orekitData==null||!orekitData.exists()){
+                throw new RuntimeException("Orekit directory was not found");
+            }
+
+            DataProvidersManager manager = DataContext.getDefault().getDataProvidersManager();
+            manager.clearProviders();
+            manager.addProvider(new DirectoryCrawler(orekitData));
+            log.info("Orekit data load from" + orekitData.getAbsolutePath());
+        }
+        catch(Exception e){
+            throw new RuntimeException("Orekit init failed" + e.getMessage(), e);
+        }
+    }
+
+    private File findOrekitDataDirectory(){
+        File dockerPath = new File("/app/orekit-data");
+        if(dockerPath.exists() && dockerPath.isDirectory()){
+            log.debug("Found orekit path at" + dockerPath.getAbsolutePath());
+            return dockerPath;
+        }
+
+        try{
+            URL resourceUrl = getClass().getClassLoader().getResource("orekit-data");
+            if(resourceUrl!=null){
+                String protocol = resourceUrl.getProtocol();
+                if("file".equals(protocol)){
+                    File resourceFile = new File(resourceUrl.toURI());
+                    if(resourceFile.exists() && resourceFile.isDirectory()){
+                        log.debug("Found orekit in classpath" + resourceFile.getAbsolutePath());
+                        return resourceFile;
+                    }
+                    else if("jar".equals(protocol)){
+                        log.debug("Orekit data found in Jar");
+                    }
+                }
+            }
+        }catch(Exception e){
+            log.debug("Could not load orekit from classpath" + e.getMessage());
+        }
+
+
+        return null;
+    }
+
+
 }
